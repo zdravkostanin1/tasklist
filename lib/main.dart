02d34e22dc3cbb2463d82032da963d1b502ac887
Zdravko <zdravko.stanin@gmail.com>
2021-08-05 14:40:14 +0300

    Added a Expandable Calendar widget at the top, as an AppBar, to represent the month, year & days of the week.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import 'package:calendar_appbar/calendar_appbar.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,6 +18,17 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        // A expandable calendar implemented as a appBar
+        appBar: CalendarAppBar(
+          onDateChanged: (value) => print(value),
+          firstDate: DateTime.now().subtract(Duration(days: 140)),
+          lastDate: DateTime.now(),
+          // colors
+          white: Colors.white,
+          black: Colors.black,
+          accent: Color(0xFF4562FE),
+        ),
+        // Scaffold color
         backgroundColor: Colors.white,
         // This is the 'Add a task floating button'
         floatingActionButton: FloatingActionButton(
